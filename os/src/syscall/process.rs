7b10745fd17b65773e1d3da77eaa989ec3b06945
scat <suzuki1968@protonmail.com>
2025-04-11 16:31:45 +0800

    add sys_get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,7 +1,8 @@
 //! Process management syscalls
-// use riscv::addr::VirtAddr;
+
 use crate::mm::VirtAddr;
-use crate::task::{append_memory_to_cur_task_memspace, unmap_memory_to_cur_task_space};
+use crate::mm::page_table::translated_ptr;
+use crate::task::{append_memory_to_cur_task_memspace, current_user_token, unmap_memory_to_cur_task_space};
 use crate::{
     config::MAX_SYSCALL_NUM,
     task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next, TaskStatus},
@@ -59,6 +60,8 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
     let us = get_time_us();
     trace!("result is {}",us);
+    // 这个syscall的任务是把获得的us写入ts，但传入的ts是虚拟地址而不是物理地址，加入翻译功能...
+    let ts = translated_ptr(current_user_token(),ts as usize) as *mut TimeVal;
     unsafe {
         *ts = TimeVal {
             sec: us / 1_000_000,
